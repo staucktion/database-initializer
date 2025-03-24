8a6626ecc2c64edd4f65355f3b2da11a661ed757
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2025-03-24 12:35:18 +0300

    update auction test data

--- test/auction-auction.sql
+++ test/auction-auction.sql
@@ -454,14 +454,12 @@ COPY public.auction (id, category_id, status_id, start_time, finish_time, is_del
 
 
 COPY public.auction_photo (id, photo_id, auction_id, status_id, last_bid_amount, start_time, finish_time, current_winner_order, winner_user_id_1, winner_user_id_2, winner_user_id_3, created_at, updated_at) FROM stdin;
-1	3	1	6	800.00	2025-03-09 13:58:40	2025-03-09 13:58:50	\N	\N	\N	\N	2025-03-09 13:58:40	2025-03-09 14:03:40
+1	1	1	6	500.00	2025-03-09 13:58:40	2025-03-09 13:58:50	\N	\N	\N	\N	2025-03-09 13:58:40	2025-03-09 14:03:40
+2	2	1	6	500.00	2025-03-09 13:58:40	2025-03-09 13:58:50	\N	\N	\N	\N	2025-03-09 13:58:40	2025-03-09 14:03:40
 \.
 
 
-COPY public.bid (bid_amount, user_id, auction_photo_id, created_at) FROM stdin;
-700.00	2	1	2025-03-09 14:03:40
-800.00	3	1	2025-03-09 14:03:40
-600.00	4	1	2025-03-09 14:03:40
+COPY public.bid (id, bid_amount, user_id, auction_photo_id, created_at) FROM stdin;
 \.
 
 
@@ -472,7 +470,7 @@ COPY public.category (id, name, status_id, address, location_id, valid_radius, i
 
 
 COPY public.cron (id, unit, "interval", last_trigger_time) FROM stdin;
-1	s	10	\N
+1	m	1	\N
 \.
 
 
@@ -484,14 +482,14 @@ COPY public.location (id, latitude, longitude) FROM stdin;
 \.
 
 
-COPY public.notification (id, sent_by_user_id, sent_to_user_id, type, message, created_at, updated_at, seen_at) FROM stdin;
+COPY public.notification (id, sent_by_user_id, sent_to_user_id, type, message, seen_at, created_at, updated_at) FROM stdin;
 \.
 
 
-COPY public.photo (id, file_path, title, user_id, auction_id, location_id, category_id, status_id, is_auctionable, device_info, vote_count, purchase_now_price, is_deleted, created_at, updated_at) FROM stdin;
-3	2025-03-04-14-18-357.jpg	2025-03-04-14-18-357.jpg	1	1	1	1	6	t	deviceInfo	100	100	f	2025-03-04 11:18:10.879	2025-03-09 14:29:40
-2	2025-03-04-14-18-357.jpg	2025-03-04-14-18-357.jpg	1	1	1	1	7	t	deviceInfo	50	100	f	2025-03-04 11:18:10.879	2025-03-09 14:29:40
-1	2025-03-04-14-18-357.jpg	2025-03-04-14-18-357.jpg	1	1	1	1	7	t	deviceInfo	10	100	f	2025-03-04 11:18:10.879	2025-03-09 14:29:40
+COPY public.photo (id, file_path, title, user_id, auction_id, location_id, category_id, status_id, is_auctionable, device_info, vote_count, purchase_now_price, purchased_at, is_deleted, created_at, updated_at) FROM stdin;
+3	3.png	3.png	1	1	1	1	7	t	deviceInfo	100	100.00	\N	f	2025-03-04 11:18:10.879	2025-03-09 14:29:40
+2	2.png	2.png	1	1	1	1	6	t	deviceInfo	50	100.00	\N	f	2025-03-04 11:18:10.879	2025-03-09 14:29:40
+1	1.png	1.png	1	1	1	1	6	t	deviceInfo	10	100.00	\N	f	2025-03-04 11:18:10.879	2025-03-09 14:29:40
 \.
 
 
@@ -545,7 +543,7 @@ SELECT pg_catalog.setval('public.auction_id_seq', 2, false);
 SELECT pg_catalog.setval('public.auction_photo_id_seq', 2, false);
 
 
-SELECT pg_catalog.setval('public.bid_id_seq', 4, false);
+SELECT pg_catalog.setval('public.bid_id_seq', 1, false);
 
 
 SELECT pg_catalog.setval('public.category_id_seq', 2, false);
